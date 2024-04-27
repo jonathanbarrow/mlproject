449df1e98b72b8ddd98e7312174383d1b195789c
jonthanbarrow <470meebarrow@gmail.com>
2024-04-27 13:22:59 -0700

    Data Ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,5 +26,25 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-        except:
-            pass
+            
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df, test_size=0.2, random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            
+            logging.info("Ingestion of the data is completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+                
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__ == '__main__':
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+    
